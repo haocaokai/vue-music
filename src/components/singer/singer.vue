a2c1ed8e05f13c7355846b3a7718af9da101e8af
haocaokai <307356224@qq.com>
2018-02-27 17:24:15 +0800

    添加歌手列表

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,7 +5,79 @@
 </template>
 
 <script>
-  
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
+
+  export default {
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList(){
+        getSingerList().then((res) => {
+          this.singers = res.data.list
+          console.log(this._normalizeSinger(this.singers));
+        })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+
+        list.forEach((item, index) => {
+          if(index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+
+          const key = item.Findex
+
+          if(!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        })
+
+        let ret = []
+        let hot = []
+
+        for(let key in map) {
+          let val = map[key]
+          if(val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          }else if(val.title === HOT_NAME){
+            hot.push(val)
+          }
+        }
+
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+
+        return hot.concat(ret)
+      }
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
