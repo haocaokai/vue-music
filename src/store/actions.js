f2012f05833313dccdebdf1e12a6a9b984265bc5
cakai <307356224@qq.com>
2018-03-11 21:05:28 +0800

    解决随机播放列表乱序问题

--- src/store/actions.js
+++ src/store/actions.js
@@ -8,12 +8,11 @@ function findIndex(list, song) {
   })
 }
 
-
 export const selectPlay = function({commit, state}, {list, index}) {
   commit(types.SET_SEQUENCE_LIST, list)
   if(state.mode === playMode.random) {
     let randomList = shuffle(list)
-    commit(types.SET_PLAY_LIST, list)
+    commit(types.SET_PLAY_LIST, randomList)
     index = findIndex(randomList, list[index])
   }else{
     commit(types.SET_PLAY_LIST, list)
@@ -22,6 +21,8 @@ export const selectPlay = function({commit, state}, {list, index}) {
   commit(types.SET_FULL_SCREEN, true)
   commit(types.SET_PLAYING_STATE, true)
 }
+
+
 
 export const randomPlay = function({commit}, {list}) {
   commit(types.SET_MODE, playMode.random)
