63e48528a8db720fd8d4bc045ad23c9585df5cb8
haocaokai <307356224@qq.com>
2018-03-12 18:53:14 +0800

    歌词滚动

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -297,7 +297,10 @@
       handleLyric({lineNum, txt}) {
         this.currentLineNum = lineNum
         if(lineNum > 5) {
-          
+          let lineEl = this.$refs.lyricLine[lineNum - 5]
+          this.$refs.lyricList.scrollToElement(lineEl, 1000)
+        }else{
+          this.$refs.lyricList.scrollTo(0, 0, 1000)
         }
       },
       _pad(num, n=2) {
